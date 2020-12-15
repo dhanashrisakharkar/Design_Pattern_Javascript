1f8f8e2b8ff372d32dd9b7deb3108676d8d406ec
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-12-15 19:35:45 +0530

    [Dhanashri] Refactor. Changes Has been done while Printing Output

--- FactoryPattern/FactoryPatternMain.js
+++ FactoryPattern/FactoryPatternMain.js
@@ -4,16 +4,27 @@
 * @Auther : Dhanashri Sakharkar
 /*********************************************************************/
 
+/*
+ *@Function:-File Data will take and print output in form of employee
+ */
 let fileData = require('./FactoryPatternBussinessLogics')
 
 const employees = []
 
-employees.push(fileData.create("dhanashri", 1));
-employees.push(fileData.create("rajeshwari", 2));
-
+employees.push(fileData.create("Dhanashri  ", 1));
+employees.push(fileData.create("Abhishek   ", 2));
+employees.push(fileData.create("Rajeshwari ",3));
+console.log("*****************" + "***********" + "***************" )
+console.log("Name of Employee" +  "  Job Type " + "         Salary" )
+console.log("*****************" + "***********" + "***************" )
 function say() {
-    console.log("hello my name is " + this.name + " and i am an " + this.type);
+    // console.log("hello my name is " + this.name + " and i am an " + this.type + " My salary is " + this.salary);
+    
+        console.log(this.name+"       "+this.type+"      "+this.salary)
+        console.log("-----------------" + "-----------" + "---------------" )
+        
 }
+
 employees.forEach(emp => {
     say.call(emp)
 })
